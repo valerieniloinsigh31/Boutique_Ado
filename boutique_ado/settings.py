06b03415cdb8b35dba01bf29f0bf140444635b3d
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-25 18:34:58 +0000

    Set up Allauth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -34,11 +34,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth', #Required by Django Allauth-here by default#
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    'django.contrib.messages', #Required by Django Allauth-here by default#
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',#Required by Django Allauth-added#
+    'allauth.account',#Required by Django Allauth-added#
+    'allauth.socialaccount',#Required by Django Allauth-added#
 ]
 
 MIDDLEWARE = [
@@ -61,13 +65,33 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', #Required by Django AllAuth so don't remove#
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
