8a2a706c15c93c0ef8e949a6d38c74f98c556bb3
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-04-29 22:09:57 +0000

    Added quantity boxes to shopping bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,8 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.model import Product
-
+from products.models import Product
 
 def bag_contents(request):
 
@@ -21,16 +20,17 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-        else: 
+        else:
+            product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price 
-                product.count += quantity
+                total += quantity * product.price
+                product_count += quantity
                 bag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-                'size': size,
-            })
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -38,9 +38,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
