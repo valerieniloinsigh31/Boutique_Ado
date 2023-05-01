61a9db7104031f90a063e6b39b963905fb09ae3a
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-01 01:02:56 +0000

    Added stripe elements

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from .models import Order
 
-class OrderForm(forms.Modelform):
+
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields= ('full_name', 'email', 'phone_number',
+        fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'county',)
 
-        def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+
 from .forms import OrderForm
 
-# Create your views here.
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form' = order_form,
+        'order_form': order_form,
+        'stripe_public_key':'',
+        'client_secret': 'test client secret',
     }
 
-    return render (request, template, context)
+    return render(request, template, context)
