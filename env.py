c0b5dbb7549c9f0843623c5149941b69833fc802
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-03 18:45:20 +0000

    Added new secret key

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ['STRIPE_SECRET_KEY'] = 'sk_live_wZYsYnE3xCccaqODN43J4hmP'
+os.environ['STRIPE_SECRET_KEY'] = 'sk_test_518sU1FEYXB9h2SqflmUoO3lArEuJr46B6o56xqVaeWJBRx5j4hxVcgPRjfBfw45kNco06Vos8IJDP4MLjcAaYxNv00M5njeBg0'
 os.environ['STRIPE_PUBLIC_KEY'] = 'pk_live_mN9tBqmPsjW1nWTyQFSnhxgb'
 
