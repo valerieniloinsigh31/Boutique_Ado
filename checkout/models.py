d30e5638f754e6bbc69b37b46ac793f64c856d00
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-06 18:43:07 +0000

    Updated checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -1,18 +1,20 @@
-import uuid 
+import uuid
 
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
+from django_countries.fields import CountryField
+
 from products.models import Product
 
-# Create your models here.
-class Order(models.Model):
 
+class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -24,8 +26,6 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
-
-
 
     def _generate_order_number(self):
         """
