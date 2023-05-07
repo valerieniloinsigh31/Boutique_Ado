33866a85d495818980dc7da940f185bf46a115cf
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-07 11:37:07 +0000

    Updated profiles -  added order history and updated checout views

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
 import os
 
 os.environ['STRIPE_SECRET_KEY'] = 'sk_live_wZYsYnE3xCccaqODN43J4hmP'
+os.environ['STRIPE_WH_SECRET'] = ''
 os.environ['STRIPE_PUBLIC_KEY'] = 'pk_live_mN9tBqmPsjW1nWTyQFSnhxgb'
 
