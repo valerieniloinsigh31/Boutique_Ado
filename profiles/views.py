166ab41ff1bf7b39e854cd40bb5f34793625f025
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-07 16:32:29 +0000

    Finished add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
-from .forms import UserProfileForm 
+from .forms import UserProfileForm
 
 from checkout.models import Order
 
 def profile(request):
-    """Display the user's profile."""
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -15,8 +15,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -28,21 +30,19 @@ def profile(request):
 
     return render(request, template, context)
 
+
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
-    messages.info(request,(
-        f'This is a past confirmation for {order_number}.'
-        'A confirmation email was sent on the confirmation date.'
-    )
-    )
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
 
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'from_profile': True, 
+        'from_profile': True,
     }
 
     return render(request, template, context)
-
-    
