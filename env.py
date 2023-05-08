1a25f16a092050faf4515e724e8ab66dd5de6c80
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-08 19:08:54 +0000

    Added Database_Url to env.py file

--- env.py
+++ env.py
@@ -3,4 +3,5 @@ import os
 os.environ['STRIPE_SECRET_KEY'] = 'sk_live_wZYsYnE3xCccaqODN43J4hmP'
 os.environ['STRIPE_WH_SECRET'] = 'whsec_bFSXWgKHtqTbfcfFOcv6ulXbPceBSbDd'
 os.environ['STRIPE_PUBLIC_KEY'] = 'pk_live_mN9tBqmPsjW1nWTyQFSnhxgb'
+os.environ.get['DATABASE_URL'] = 'postgres://ftmpxxrc:IkQQAWwYjBae3fe6j_b7QpK6X7Z2-uqi@horton.db.elephantsql.com/ftmpxxrc'
 
