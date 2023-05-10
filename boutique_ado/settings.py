3ccc0878b44339d23811dd616f07a1e581ca2015
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-10 11:28:37 +0000

    Updated debug to Development in Settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['https://boutiqueadopp5.herokuapp.com', 'localhost']
 
